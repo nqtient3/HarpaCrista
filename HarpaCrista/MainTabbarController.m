3a867077fd7f28c8c36facaddfbafc76ec9cc396
Chinh Le <levanchinh11890@gmail.com>
2016-03-03 15:13:24 +0700

    Fix Tabbar icons issue

--- HarpaCrista/MainTabbarController.m
+++ HarpaCrista/MainTabbarController.m
@@ -17,13 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.mainTabBarController setTintColor:[UIColor colorWithRed:80/255.f green:187/255.f blue:250/255.f alpha:1.0]];
+//    [self.mainTabBarController setTintColor:[UIColor colorWithRed:80/255.f green:187/255.f blue:250/255.f alpha:1.0]];
+    
     UITabBarItem *tabBarHinosItem = [self.tabBar.items objectAtIndex:0];
-    tabBarHinosItem.selectedImage = [UIImage imageNamed:@"ic_hinos"];
+    tabBarHinosItem.selectedImage = [[UIImage imageNamed:@"ic_hinos_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     UITabBarItem *tabBarFavoritosItem = [self.tabBar.items objectAtIndex:1];
-    tabBarFavoritosItem.selectedImage = [UIImage imageNamed:@"ic_star"];
+    tabBarFavoritosItem.selectedImage = [[UIImage imageNamed:@"ic_star_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     UITabBarItem *tabBarMaisItem = [self.tabBar.items objectAtIndex:2];
-    tabBarMaisItem.selectedImage = [UIImage imageNamed:@"ic_global"];
+    tabBarMaisItem.selectedImage = [[UIImage imageNamed:@"ic_global_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 }
 
 - (void)didReceiveMemoryWarning {
