1a57071c81edfadecc760c8a69265b9c79aa9914
chinhle <levanchinh11890@gmail.com>
2016-03-12 14:27:01 +0700

    Add Push Notification code

--- HarpaCrista/AppDelegate.h
+++ HarpaCrista/AppDelegate.h
@@ -8,10 +8,12 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
+#import <OneSignal/OneSignal.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) OneSignal* oneSignal;
 
 @property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
