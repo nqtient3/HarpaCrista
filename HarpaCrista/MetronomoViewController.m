f0ed49b173227f382276c24dd16235c3b7011321
TienNgo <nqtient3@gmail.com>
2016-03-12 19:46:30 +0700

    Update codes for metronomes

--- HarpaCrista/MetronomoViewController.m
+++ HarpaCrista/MetronomoViewController.m
@@ -23,7 +23,17 @@
     __weak IBOutlet UIButton *tone34Button;
     __weak IBOutlet UIButton *tone24Button;
     __weak IBOutlet UIButton *tone68Button;
+    __weak IBOutlet UIButton *beat1Button;
+    __weak IBOutlet UIButton *beat2Button;
+    __weak IBOutlet UIButton *beat3Button;
+    __weak IBOutlet UIButton *beat4Button;
+    __weak IBOutlet UIButton *beat5Button;
+    __weak IBOutlet UIButton *beat6Button;
+    __weak IBOutlet UIButton *beat7Button;
+    __weak IBOutlet UIButton *beat8Button;
     Metronome *currentMetronome;
+    NSTimer *myTime;
+    int count;
 }
 
 @end
@@ -35,6 +45,12 @@
     // Do any additional setup after loading the view.
     self.title = @"Metrónomo";
     slider.value = 10;
+    tone44Button.selected = YES;
+    beat5Button.hidden = YES;
+    beat6Button.hidden = YES;
+    beat7Button.hidden = YES;
+    beat8Button.hidden = YES;
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -46,24 +62,49 @@
 
 - (IBAction)tone44Action:(UIButton *)sender {
     sender.selected =! sender.selected;
-}
-
-#pragma mark - discreaseBPMButton
-
-- (IBAction)tone24Action:(UIButton *)sender {
-    sender.selected =! sender.selected;
+    if (sender.selected) {
+        beat5Button.hidden = YES;
+        beat6Button.hidden = YES;
+        beat7Button.hidden = YES;
+        beat8Button.hidden = YES;
+        
+    }
 }
 
 #pragma mark - discreaseBPMButton
 
 - (IBAction)tone34Action:(UIButton *)sender {
     sender.selected =! sender.selected;
+    if (sender.selected) {
+        beat5Button.hidden = YES;
+        beat6Button.hidden = YES;
+        beat7Button.hidden = YES;
+        beat8Button.hidden = YES;
+    }
+}
+
+#pragma mark - discreaseBPMButton
+
+- (IBAction)tone24Action:(UIButton *)sender {
+    sender.selected =! sender.selected;
+    if (sender.selected) {
+        beat5Button.hidden = YES;
+        beat6Button.hidden = YES;
+        beat7Button.hidden = YES;
+        beat8Button.hidden = YES;
+    }
 }
 
 #pragma mark - discreaseBPMButton
 
 - (IBAction)tone68Action:(UIButton *)sender {
     sender.selected =! sender.selected;
+    if (sender.selected) {
+        beat5Button.hidden = NO;
+        beat6Button.hidden = NO;
+        beat7Button.hidden = NO;
+        beat8Button.hidden = NO;
+    }
 }
 
 #pragma mark - increaseBPMButton
@@ -106,9 +147,58 @@
     sender.selected = !sender.selected;
     if (sender.selected) {
         [currentMetronome start];
+        myTime = [NSTimer scheduledTimerWithTimeInterval: 60.0/slider.value
+                                                  target: self
+                                                selector: @selector (updateBackgroundButton:)
+                                                userInfo: nil
+                                                 repeats: YES];
     } else {
         [currentMetronome stop];
+        if (myTime != nil) {
+            [myTime invalidate];
+            myTime = nil;
+        }
     }
+}
+
+- (void)updateBackgroundButton:(NSTimer *)theTimer {
+    count = count + 1;
+    [UIView beginAnimations: nil context: nil];
+    [UIView setAnimationDuration: 60.0/slider.value];
+    if (count == 5) {
+        count = 1;
+    }
+        if (count == 1) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                beat1Button.selected = YES;
+                beat2Button.selected = NO;
+                beat3Button.selected = NO;
+                beat4Button.selected = NO;
+            });
+        } else if (count == 2) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                beat1Button.selected = NO;
+                beat2Button.selected = YES;
+                beat3Button.selected = NO;
+                beat4Button.selected = NO;
+            });
+        } else if (count == 3) {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                beat1Button.selected = NO;
+                beat2Button.selected = NO;
+                beat3Button.selected = YES;
+                beat4Button.selected = NO;
+             });
+        } else if (count == 4) {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                beat1Button.selected = NO;
+                beat2Button.selected = NO;
+                beat3Button.selected = NO;
+                beat4Button.selected = YES;
+             });
+        }
+
+        [UIView commitAnimations];
 }
 
 #pragma mark - Alter Metronome
