9c69dff8548691b40f8386eac5b6633109bccf98
TienNgo <nqtient3@gmail.com>
2016-03-18 11:10:49 +0700

    Add Tuner

--- HarpaCrista/TunerViewController.h
+++ HarpaCrista/TunerViewController.h
@@ -7,26 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "GTNote.h"
-#import "PitchDetector.h"
-#import "RWKnobControl.h"
 
-@interface TunerViewController : UIViewController < UIPopoverControllerDelegate>
 
-@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (strong, nonatomic) UIPopoverController *flipsidePopoverController;
-@property (strong, nonatomic) RWKnobControl *knobControl;
-@property (strong, nonatomic) UIView *knobPlaceholder;
-@property (strong, nonatomic) GTNote *noteData;
-@property (nonatomic, strong) UILabel *noteDisplay;
-@property (nonatomic, strong) UILabel *freqencyDisplay;
-@property (nonatomic, assign) double currentFrequency;
-@property (nonatomic, strong) NSString *currentNote;
-@property(assign) BOOL isListening;
-
-@property (nonatomic, strong) PitchDetector *pitchDetector;
-
-- (void)updateFrequencyLabel;
-- (void)updateToFrequncy:(double)freqency;
+@interface TunerViewController : UIViewController
 
 @end
